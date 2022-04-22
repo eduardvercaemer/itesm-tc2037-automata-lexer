f80c6440ad4552f64faa470b4c4e9f2f2b34046a
Vercaemer Eduard <vercaemereduard@gmail.com>
2022-04-21 21:11:04 -0500

    add: lex comments after assignments

--- src/lexer/core.clj
+++ src/lexer/core.clj
@@ -30,6 +30,7 @@
     :expr-num                [{:where :end :to :halt :action [:check-paren :out-num]}
                               {:where :num :to :expr-num :action :eat}
                               {:where :newline :to :stmt :action [:check-paren :out-num]}
+                              {:where :slash :to :expr-cmt-0 :action [:out-num :eat]}
                               {:where :op :to :asg-expr :action [:out-num :eat :out-op]}
                               {:where :cparen :to :expr-op :action [:del-paren :out-num :out-cparen]}
                               {:where :ws :to :expr-op :action :out-num}]
@@ -37,14 +38,22 @@
     :expr-token              [{:where :end :to :halt :action [:check-paren :out-token]}
                               {:where :irest :to :expr-token :action :eat}
                               {:where :newline :to :stmt :action [:check-paren :out-token]}
+                              {:where :slash :to :expr-cmt-0 :action [:out-num :eat]}
                               {:where :op :to :asg-expr :action [:out-token :eat :out-op]}
                               {:where :cparen :to :expr-op :action [:del-paren :out-token :out-cparen]}
                               {:where :ws :to :expr-op :action :out-token}]
+    ;; expressions can end optionally by comments, which can be a little confused by division '/'
+    :expr-cmt-0              [{:where :slash :to :cmt-1 :action [:check-paren :eat]}
+                              {:where :num :to :expr-num :action [:out-op :eat]}
+                              {:where :istart :to :expr-token :action [:out-op :eat]}
+                              {:where :oparen :to :asg-expr :action [:out-op :add-paren :out-oparen]}
+                              {:where :ws :to :asg-expr :action :out-op}]
     ;; separated by operators
     :expr-op                 [{:where :end :to :halt :action :check-paren}
                               {:where :ws :to :expr-op}
                               {:where :newline :to :stmt :action :check-paren}
                               {:where :cparen :to :expr-op :action [:del-paren :out-cparen]}
+                              {:where :slash :to :expr-cmt-0 :action :eat}
                               {:where :op :to :asg-expr :action [:eat :out-op]}]}})
 
 (defn -main
@@ -54,5 +63,9 @@
                a = 5
                
                _678=(a * 2) + (6 / _2 * (3 - 1))
-               value = 35"]
+               value = 35 // with comment at the end !
+               value = 65 + a // also after tokens !
+               value = 9 + (xyz)//or with no spaces !
+               value = foo / bar
+               value = 5 * (6 / (2) // fails on bad parenthesis !"]
     (run language input)))
