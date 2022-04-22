bdd9420f390cec7184cd80d29389191907df02f6
Vercaemer Eduard <vercaemereduard@gmail.com>
2022-04-21 21:02:12 -0500

    add: tokens in expressions

--- src/lexer/core.clj
+++ src/lexer/core.clj
@@ -6,14 +6,14 @@
   {:start :stmt
    :transitions
    {;; statements, are either empty, comments, or assignemnts
-    :stmt                    [{:where [:ws :newline] :to :stmt}
-                              {:where :end :to :halt}
+    :stmt                    [{:where :end :to :halt}
+                              {:where [:ws :newline] :to :stmt}
                               {:where :slash :to :cmt-0 :action :eat}
                               {:where :istart :to :asg-token :action :eat}]
     ;; comments begin with two slashes and end with a newline
     :cmt-0                   [{:where :slash :to :cmt-1 :action :eat}]
-    :cmt-1                   [{:where :newline :to :stmt :action :out-comment}
-                              {:where :end :to :halt :action :out-comment}
+    :cmt-1                   [{:where :end :to :halt :action :out-comment}
+                              {:where :newline :to :stmt :action :out-comment}
                               {:where :any :to :cmt-1 :action :eat}]
     ;; assignments begin with a token
     :asg-token               [{:where :irest :to :asg-token :action :eat}
@@ -23,23 +23,36 @@
     :asg-equal               [{:where :equal :to :asg-expr :action :out-equal}]
     ;; then have an expression
     :asg-expr                [{:where :num :to :expr-num :action :eat}
+                              {:where :istart :to :expr-token :action :eat}
                               {:where :oparen :to :asg-expr :action [:add-paren :out-oparen]}
                               {:where :ws :to :asg-expr}]
     ;; expressions have numeric parts
-    :expr-num                [{:where :num :to :expr-num :action :eat}
+    :expr-num                [{:where :end :to :halt :action [:check-paren :out-num]}
+                              {:where :num :to :expr-num :action :eat}
                               {:where :newline :to :stmt :action [:check-paren :out-num]}
-                              {:where :end :to :halt :action [:check-paren :out-num]}
                               {:where :op :to :asg-expr :action [:out-num :eat :out-op]}
                               {:where :cparen :to :expr-op :action [:del-paren :out-num :out-cparen]}
                               {:where :ws :to :expr-op :action :out-num}]
+    ;; expressions can also have tokens instead of a numeric part
+    :expr-token              [{:where :end :to :halt :action [:check-paren :out-token]}
+                              {:where :irest :to :expr-token :action :eat}
+                              {:where :newline :to :stmt :action [:check-paren :out-token]}
+                              {:where :op :to :asg-expr :action [:out-token :eat :out-op]}
+                              {:where :cparen :to :expr-op :action [:del-paren :out-token :out-cparen]}
+                              {:where :ws :to :expr-op :action :out-token}]
     ;; separated by operators
-    :expr-op                 [{:where :ws :to :expr-op}
+    :expr-op                 [{:where :end :to :halt :action :check-paren}
+                              {:where :ws :to :expr-op}
                               {:where :newline :to :stmt :action :check-paren}
-                              {:where :end :to :halt :action :check-paren}
                               {:where :cparen :to :expr-op :action [:del-paren :out-cparen]}
                               {:where :op :to :asg-expr :action [:eat :out-op]}]}})
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& _]
-  (run language " // this is a comment\n_78=((((2))))\n\na=5+7\nabc = 5 + (7)\nab = ( 5 )\nn = ( 7 / (2+3))"))
+  (let [input "// this is a comment
+               a = 5
+               
+               _678=(a * 2) + (6 / _2 * (3 - 1))
+               value = 35"]
+    (run language input)))
--- src/lexer/run.clj
+++ src/lexer/run.clj
@@ -57,31 +57,35 @@
 (defn- match-symbol
   "Defines the different kinds of symbols"
   [where symbol]
-  (case where
-    :ws (= symbol \ )
-    :newline (= symbol \newline)
-    :one (= symbol \1)
-    :equal (= symbol \=)
-    :slash (= symbol \/)
-    :oparen (= symbol \()
-    :cparen (= symbol \))
-    :op (contains? #{\+ \* \- \/ \^} symbol)
-    :num (and (>= (int symbol) (int \0)) (<= (int symbol) (int \9)))
-    :lower (and (>= (int symbol) (int \a)) (<= (int symbol) (int \z)))
-    :upper (and (>= (int symbol) (int \A)) (<= (int symbol) (int \Z)))
-    :under (= symbol \_)
-    :alpha (or (match-symbol :lower symbol) (match-symbol :upper symbol))
-    :istart (or (match-symbol :alpha symbol) (match-symbol :under symbol))
-    :irest (or (match-symbol :istart symbol) (match-symbol :num symbol))
-    :any true
-    :end (= symbol nil)))
+  (if (nil? symbol)
+    (= where :end)
+    (case where
+      :ws (= symbol \ )
+      :newline (= symbol \newline)
+      :one (= symbol \1)
+      :equal (= symbol \=)
+      :slash (= symbol \/)
+      :oparen (= symbol \()
+      :cparen (= symbol \))
+      :op (contains? #{\+ \* \- \/ \^} symbol)
+      :num (and (>= (int symbol) (int \0)) (<= (int symbol) (int \9)))
+      :lower (and (>= (int symbol) (int \a)) (<= (int symbol) (int \z)))
+      :upper (and (>= (int symbol) (int \A)) (<= (int symbol) (int \Z)))
+      :under (= symbol \_)
+      :alpha (or (match-symbol :lower symbol) (match-symbol :upper symbol))
+      :istart (or (match-symbol :alpha symbol) (match-symbol :under symbol))
+      :irest (or (match-symbol :istart symbol) (match-symbol :num symbol))
+      :any true
+      :end false
+      (throw (new Exception "invalid 'where' keyword")))))
 
 (defn- transition-matches?
   "Wheter a transition matches a symbol or not"
   [symbol {where :where}]
   (cond
     (vector? where) (first (filter #(match-symbol % symbol) where))
-    (keyword? where) (match-symbol where symbol)))
+    (keyword? where) (match-symbol where symbol)
+    :else (throw (new Exception "invalid 'where' kind"))))
 
 (defn- find-transition
   "Find the first valid transition for a symbol"
